dd420317984713d125be3c27734635e2cb89d77c
pauliescanlon <pauliescanlon@gmail.com>
2020-01-01 14:13:40 +0000

    remove flickr loading stuff

--- @pauliescanlon/gatsby-mdx-embed/src/components/Flickr/Flickr.js
+++ @pauliescanlon/gatsby-mdx-embed/src/components/Flickr/Flickr.js
@@ -1,34 +1,21 @@
-import React, { useState } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 
-export const Flickr = ({ id }) => {
-  const [hasImageloaded, setHasImageLoaded] = useState(false)
-
-  return (
-    <span
-      className="flickr-embed-mdx"
-      data-flickr-embed="true"
-      data-header="true"
-      data-footer="true"
-    >
-      {typeof window !== "undefined" &&
-      !window.FlickrEmbedr &&
-      !hasImageloaded ? (
-        <blockquote>
-          <a href={`https://live.staticflickr.com/${id}`}>Loading</a>
-        </blockquote>
-      ) : null}
-      {/* eslint-disable-next-line */}
-      <img
-        src={`https://live.staticflickr.com/${id}`}
-        width="100%"
-        height="auto"
-        alt={id}
-        onLoad={() => setHasImageLoaded(true)}
-      />
-    </span>
-  )
-}
+export const Flickr = ({ id }) => (
+  <span
+    className="flickr-embed-mdx"
+    data-flickr-embed="true"
+    data-header="true"
+    data-footer="true"
+  >
+    <img
+      src={`https://live.staticflickr.com/${id}`}
+      width="100%"
+      height="auto"
+      alt={id}
+    />
+  </span>
+)
 
 Flickr.propTypes = {
   id: PropTypes.string.isRequired,
