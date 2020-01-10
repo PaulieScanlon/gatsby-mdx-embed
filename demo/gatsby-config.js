8e4e6762eb6d4bb92c130720645b400965809c98
pauliescanlon <pauliescanlon@gmail.com>
2020-01-10 09:32:17 +0000

    add google analytics

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: [`@pauliescanlon/gatsby-mdx-embed`, `gatsby-theme-docz`],
+  plugins: [
+    `@pauliescanlon/gatsby-mdx-embed`,
+    `gatsby-theme-docz`,
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-76055934-5",
+      },
+    },
+  ],
 }
