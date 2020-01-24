4db309e33f732612ef353b2df1b3aa6511872c67
pauliescanlon <pauliescanlon@gmail.com>
2020-01-24 21:31:21 +0000

    add tsconfig at root to solve propTables issue ffs

--- demo/doczrc.js
+++ demo/doczrc.js
@@ -1,17 +1,5 @@
 const path = require("path")
 
-// path to tsx
-// const directoryPath = path.join(
-//   process.cwd(),
-//   "../@pauliescanlon/gatsby-mdx-embed/src/components"
-// )
-
-//path to js created by babel
-const directoryPath = path.join(
-  process.cwd(),
-  "../@pauliescanlon/gatsby-mdx-embed/components"
-)
-
 export default {
   title: "gatsby-mdx-embed",
   description:
@@ -19,18 +7,12 @@ export default {
   base: "https://gatsby-mdx-embed.netlify.com/",
   ogImage: "mdx-embed-main-og-image.jpg",
   menu: ["Getting Started"],
-  // typescript: true,
-  // filterComponents: files => {
-  //   // console.log("files: ", files)
-  //   console.log(
-  //     "filter: ",
-  //     files.filter(filepath => !/index.(js|jsx|ts|tsx)$/.test(filepath))
-  //   )
-
-  //   return files.filter(filepath => !/index.(js|jsx|ts|tsx)$/.test(filepath))
-  // },
+  typescript: true,
   docgenConfig: {
-    searchPath: directoryPath,
+    searchPath: path.resolve(
+      __dirname,
+      "../@pauliescanlon/gatsby-mdx-embed/src/components"
+    ),
   },
   themeConfig: {
     initialColorMode: "light",
