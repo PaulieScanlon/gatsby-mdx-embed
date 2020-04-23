dd5378bce337fbec3a48ffc034d05c3b4d2f4575
Zach Schnackel <info@zslabs.com>
2020-04-23 11:24:16 -0400

    Add collection and channel query param support to Twitch

--- @pauliescanlon/gatsby-mdx-embed/src/components/Twitch/Twitch.tsx
+++ @pauliescanlon/gatsby-mdx-embed/src/components/Twitch/Twitch.tsx
@@ -12,20 +12,37 @@ export interface ITwitchProps {
     s: number
   }
   /** Auto play the video */
-  autoPlay: boolean
+  autoPlay: boolean,
+  /** Name of the channel, for a live stream */
+  channel: string,
+  /** Collection ID, for a collection of videos */
+  collection: string
 }
 
 export const Twitch: FunctionComponent<ITwitchProps> = ({
   twitchId,
   autoPlay = false,
-  skipTo = { h: 0, m: 0, s: 0 }
+  skipTo = { h: 0, m: 0, s: 0 },
+  channel = '',
+  collection = ''
 }: ITwitchProps) => {
   const { h, m, s } = skipTo
 
   return (
-    <GeneralObserver>
-      <div
-        className="twitch-mdx-embed"
+    <div
+      className="twitch-mdx-embed"
+      style={{
+        position: 'relative',
+        width: '100%',
+        ...getPadding('16:9')
+      }}
+    >
+      <iframe
+        title={`twitch-${twitchId}`}
+        src={`https://player.twitch.tv/?autoplay=${autoPlay}&t=${h}h${m}m${s}s&video=v${twitchId}&channel=${channel}&collection=${collection}`}
+        frameBorder="0"
+        allow="autoplay; fullscreen"
+        allowFullScreen
         style={{
           position: 'relative',
           width: '100%',
