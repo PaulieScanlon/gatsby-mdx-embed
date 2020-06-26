0ffa57ce9e7d901f389d9c937c342f16f2c4348d
spences10 <spences10apps@gmail.com>
2020-06-26 20:07:10 +0100

    add components for TikTok

--- @pauliescanlon/gatsby-mdx-embed/src/index.js
+++ @pauliescanlon/gatsby-mdx-embed/src/index.js
@@ -1,25 +1,26 @@
-export { MdxEmbedProvider } from './components/MdxEmbedProvider'
 export { CodePen } from './components/CodePen'
 export { CodeSandbox } from './components/CodeSandbox'
 export { Flickr } from './components/Flickr'
+export { Gist } from './components/Gist'
 export { Instagram } from './components/Instagram'
-export { Spotify } from './components/Spotify'
-export {
-  Tweet,
-  TwitterTimeline,
-  TwitterList,
-  TwitterFollowButton,
-  TwitterMentionButton,
-  TwitterHashtagButton
-} from './components/Twitter'
-export { Vimeo } from './components/Vimeo'
-export { YouTube } from './components/YouTube'
+export { MdxEmbedProvider } from './components/MdxEmbedProvider'
 export {
   Pin,
   PinterestBoard,
   PinterestFollowButton
 } from './components/Pinterest'
 export { SoundCloud } from './components/SoundCloud'
-export { Gist } from './components/Gist'
+export { Spotify } from './components/Spotify'
+export { TikTok } from './components/TikTok'
 export { Twitch } from './components/Twitch'
+export {
+  Tweet,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterList,
+  TwitterMentionButton,
+  TwitterTimeline
+} from './components/Twitter'
+export { Vimeo } from './components/Vimeo'
 export { Wikipedia } from './components/Wikipedia'
+export { YouTube } from './components/YouTube'
