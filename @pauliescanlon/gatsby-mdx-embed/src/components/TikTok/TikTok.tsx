ef537cebaf804a98a51ec54c9d19247330961211
pauliescanlon <pauliescanlon@gmail.com>
2020-07-29 13:13:34 +0100

    refactor TikTok component - update help stories

--- @pauliescanlon/gatsby-mdx-embed/src/components/TikTok/TikTok.tsx
+++ @pauliescanlon/gatsby-mdx-embed/src/components/TikTok/TikTok.tsx
@@ -7,20 +7,23 @@ export interface ITikTokProps {
 }
 
 export const TikTok: FunctionComponent<ITikTokProps> = ({
-  tikTokId: tikTokId
-}: ITikTokProps) => (
-  <GeneralObserver onEnter={() => handleTikTokLoad()}>
-    <blockquote
-      className="tiktok-embed _embed_video_video_wrapper"
-      cite={`https://www.tiktok.com/@scout2015/video/6718335390845095173", "data-video-id": "6718335390845095173`}
-    >
-      <section>
-        <a href={`https://tiktok.com/@scout2015/video/${tikTokId}`}>
-          {typeof window !== 'undefined' && !(window as any).tiktok
-            ? 'Loading'
-            : ''}
-        </a>
-      </section>
-    </blockquote>
-  </GeneralObserver>
-)
+  tikTokId
+}: ITikTokProps) => {
+  return (
+    <GeneralObserver onEnter={() => handleTikTokLoad()}>
+      <blockquote
+        className="tiktok-embed"
+        cite={`https://www.tiktok.com/${tikTokId}`}
+        data-video-id={tikTokId.split('/').pop()}
+      >
+        <section>
+          <a href={`https://tiktok.com/${tikTokId}`}>
+            {typeof window !== 'undefined' && !(window as any).tiktok
+              ? 'Loading'
+              : ''}
+          </a>
+        </section>
+      </blockquote>
+    </GeneralObserver>
+  )
+}
