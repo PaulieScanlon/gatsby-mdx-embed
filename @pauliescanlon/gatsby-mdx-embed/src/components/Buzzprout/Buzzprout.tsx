3e3b05949cb425803ff1fb7a1161b2e81250e893
pauliescanlon <pauliescanlon@gmail.com>
2020-08-10 09:13:23 +0100

    add meta tag

--- @pauliescanlon/gatsby-mdx-embed/src/components/Buzzprout/Buzzprout.tsx
+++ @pauliescanlon/gatsby-mdx-embed/src/components/Buzzprout/Buzzprout.tsx
@@ -13,12 +13,12 @@ export const Buzzprout: FunctionComponent<IBuzzproutProps> = ({
     <GeneralObserver>
       <div style={{ position: 'relative' }}>
         <iframe
+          title={`buzzprout-${buzzproutId}`}
           src={`https://buzzsprout.com/${buzzproutId}?client_source=admin&amp;iframe=true`}
           width="100%"
           height="200"
           frameBorder="0"
           scrolling="no"
-          title="Podcast Episode"
         />
       </div>
     </GeneralObserver>
