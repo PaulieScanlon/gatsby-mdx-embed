f44f2f63347928f43750ef612e846e97b5e91577
Mike Bifulco <mbifulco@live.com>
2020-08-12 10:28:50 -0400

    switch to activityId

--- @pauliescanlon/gatsby-mdx-embed/src/components/Strava/Strava.tsx
+++ @pauliescanlon/gatsby-mdx-embed/src/components/Strava/Strava.tsx
@@ -1,26 +1,27 @@
 import React, { FunctionComponent } from 'react'
 import { GeneralObserver } from '../GeneralObserver'
 export interface IStravaProps {
-  /** The embed URL provided by strava on the activity page */
-  stravaEmbedUrl: string
+  /** The activity ID provided by Strava's share action on the activity page.  Just the text after `https://www.strava.com/activities/`, which should look something like `1234567/embed/abcdefgh` */
+  activityId: string
 }
 
 export const Strava: FunctionComponent<IStravaProps> = ({
-  stravaEmbedUrl
+  activityId
 }: IStravaProps) => {
   return (
     <GeneralObserver>
       <iframe
-        title={`strava-${stravaEmbedUrl}`}
+        title={`strava-${activityId}`}
         className="strava-mdx-embed"
         height="405"
         style={{
+          maxWidth: '590px',
           width: '100%'
         }}
         frameBorder="0"
         allowTransparency
         scrolling="no"
-        src={stravaEmbedUrl}
+        src={`https://www.strava.com/activities/${activityId}`}
       />
     </GeneralObserver>
   )
